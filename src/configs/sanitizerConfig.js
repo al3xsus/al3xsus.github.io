fdfc2371c46c5611371a1173739e0516fd39ff3e
Alexandr <al3xsus@pm.me>
2025-10-27 23:13:37 +0500

    feat: add RSS icon to SocialLink component; implement sanitizer configuration for improved HTML sanitization in posts and RSS feed generation

---
+++ src/configs/sanitizerConfig.js
@@ -0,0 +1,23 @@
+const sanitizerConfig = (defaults) => ({
+    allowedTags: defaults.allowedTags.concat([
+        "h1", "h2", "h3", "h4", "h5", "h6", "pre", "code", "img", 
+        "table", "thead", "tbody", "tr", "td", "th", "dl", "dt", 
+        "dd", "input", "cite", "sup" // Added for modern rich content
+    ]),
+    allowedAttributes: {
+        ...defaults.allowedAttributes,
+        img: ["src", "alt", "width", "height"],
+        code: ["class"],
+        a: ["href", "name", "target", { name: "rel", allowedValues: ["noopener", "noreferrer", "nofollow"] }], // Enforce secure 'rel'
+        td: ["colspan", "rowspan"],
+        th: ["colspan", "rowspan"],
+        input: ["type", "disabled", "checked"] // Required for task lists
+    },
+    allowedSchemes: ["https", "mailto", "data"], // Removed 'http' for security
+    allowedSchemesByTag: {
+        img: ["http", "https", "data"],
+    },  
+    allowProtocolRelative: true,
+});
+
+export default sanitizerConfig;
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,6 +2,7 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import sanitizeHtml from "sanitize-html";
 import MarkdownIt from "markdown-it";
+import sanitizerConfig from "../configs/sanitizerConfig";
 const parser = new MarkdownIt();
 
 export const GET = async (context) => {
@@ -16,38 +17,10 @@ export const GET = async (context) => {
     items: posts.map((post) => ({
       link: `/posts/${post.data.slug}/`,
       // Note: this will not process components or JSX expressions in MDX files.
-      content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat([
-          "h1",
-          "h2",
-          "h3",
-          "h4",
-          "h5",
-          "h6",
-          "pre",
-          "code",
-          "img",
-          "table",
-          "thead",
-          "tbody",
-          "tr",
-          "td",
-          "th",
-        ]),
-        allowedAttributes: {
-          ...sanitizeHtml.defaults.allowedAttributes,
-          img: ["src", "alt", "width", "height"],
-          code: ["class"],
-          a: ["href", "name", "target", "rel"],
-          td: ["colspan", "rowspan"],
-          th: ["colspan", "rowspan"],
-        },
-        allowedSchemes: ["http", "https", "mailto", "data"],
-        allowedSchemesByTag: {
-          img: ["http", "https", "data"],
-        },
-        allowProtocolRelative: true,
-      }),
+      content: sanitizeHtml(
+        parser.render(post.body),
+        sanitizerConfig(sanitizeHtml.defaults)
+      ),
       ...post.data,
     })),
     customData: `<language>en-us</language>`,
