f2ded8c7d26771cd9f77222984ae21909c12f39f
Alexandr <al3xsus@pm.me>
2025-10-29 15:40:49 +0500

    feat: add sitemap integration and robots.txt generation; update configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,15 @@ import { remarkReadingTime } from './remark-reading-time.mjs';
 
 import preact from '@astrojs/preact';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://al3xsus.github.io',
   base: '/',
 
-  integrations: [preact({ compat: true, devtools: true })],
+  integrations: [preact({ compat: true, devtools: true }), sitemap()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
